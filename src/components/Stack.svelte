f200a6aff5cdb50fc0eaf8f72b88cc705f707339
Dariyoush Käther <d.kaether@googlemail.com>
2024-08-14 00:32:23 +0200

    feat: overall stack optimizations * implement outputs modal with outputs data + column in the table * temporary log resources data for detailed investigation / development * fix issue when `latest` property is `Latest` (reason unknown, either because the encountered incident was an imported stack, either because it was originally an app.pulumi.com backend hosted stack, either because it is an older stack)

--- src/components/Stack.svelte
+++ src/components/Stack.svelte
@@ -1,8 +1,14 @@
 <script>
     import { onMount } from "svelte";
+    import OutputsModal from "./OutputsModal.svelte";
+
     export let fileKey = "";
 
     let data;
+    let resources = [];
+    let showModal = false;
+    let selectedOutputs = {};
+
     onMount(async function () {
         const response = await fetch("/api/state", {
             method: "POST",
@@ -11,7 +17,21 @@
         });
         const json = await response.json();
         data = JSON.parse(json.data);
+        console.debug('▶️ Resources list data:', data);
+
+        const resourcesKey = data.checkpoint.latest ? 'latest' : (data.checkpoint.Latest ? 'Latest' : null);
+        resources = data.checkpoint[resourcesKey].resources;
     });
+
+    function openModal(outputs) {
+        selectedOutputs = outputs;
+        showModal = true;
+    }
+
+    function closeModal() {
+        showModal = false;
+        selectedOutputs = {};
+    }
 </script>
 
 <article aria-busy={fileKey.trim() !== "" ? false : true}>
@@ -23,17 +43,21 @@
                 <th scope="col">Type</th>
                 <th scope="col">Created</th>
                 <th scope="col">Modified</th>
+                <th scope="col">Outputs</th>
             </tr>
         </thead>
         <tbody>
             {#if data}
-                {#each data.checkpoint.latest.resources as resource}
+                {#each resources as resource}
                     {#if resource.id && !resource.type.includes("providers")}
                         <tr>
                             <td>{resource.id}</td>
                             <td>{resource.type}</td>
                             <td>{resource.created}</td>
                             <td>{resource.modified}</td>
+                            <td>
+                                <button on:click={() => openModal(resource.outputs)}>Show</button>
+                            </td>
                         </tr>
                     {/if}
                 {/each}
@@ -42,9 +66,16 @@
     </table>
 </article>
 
+<OutputsModal show={showModal} outputs={selectedOutputs} closeModal={closeModal} />
+
 <style>
     article {
         text-align: center;
         margin: 0;
+    }
+
+    table {
+        width: 100%;
+        overflow-x: auto;
     }
 </style>
