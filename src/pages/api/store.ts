5a46a69036d44f55f00d2927dd509b136f1f6760
Harry Vince <harryavince@gmail.com>
2025-04-11 21:11:04 +0100

    chore: delete everything

--- src/pages/api/store.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro"
-
-export const get: APIRoute = ({ params, request }) => {
-  return {
-    body: JSON.stringify({
-      name: import.meta.env.BUCKET,
-    }),
-  }
-}
--- src/server/utils.ts
+++
@@ -1,5 +0,0 @@
-import { S3Helper } from '../utils/s3';
-import { Helper } from '../utils/helper';
-
-export const S3Controller = new S3Helper(import.meta.env.BUCKET, import.meta.env.DIRECTORY);
-export const HelperController = new Helper();
